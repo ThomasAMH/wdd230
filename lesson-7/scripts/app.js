4be6d785f06722363f40b784996bcdaf260dfcdc
Thomas Montoya <thomasamh2899@gmail.com>
2023-06-03 22:07:29 +0200

    Completed lesson 7

---
+++ lesson-7/scripts/app.js
@@ -0,0 +1,12 @@
+let imagesToLoad = document.querySelectorAll("img[data-src");
+
+const loadImage = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src")
+    }
+}
+
+imagesToLoad.forEach((img) => {
+    loadImage(img);
+});
